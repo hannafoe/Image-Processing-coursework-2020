4197e9731eca0163b1e705551a652e9b037cdb50
hannafoe <hana.foerster@gmail.com>
2020-12-29 16:59:10 +0100

    Just red color ray

--- Problem1.py
+++ Problem1.py
@@ -5,12 +5,11 @@ import random
 def problem1(darkening_coef,blending_coef,mode):
     #read an image from the specified file
     #Later change this to arbitrary input image file
-    img = cv2.imread('./face2.jpg',cv2.IMREAD_COLOR)
+    img = cv2.imread('./face1.jpg',cv2.IMREAD_COLOR)
     if not img is None:
         #cv2.imshow('Original image',img)
         rows,cols,channels = img.shape
         #Make image darker
-        darkening_coef = 0.6 #Change with user input later, between 0 and 1
         for x in range(rows):
             for y in range(cols):
                 for c in range(channels):
@@ -65,62 +64,143 @@ def problem1(darkening_coef,blending_coef,mode):
         else:
             end_of_face=(end_of_face/400)-0.08
         print(end_of_face)
-############CREATE SIMPLE MASK##########################################
-        if mode=='simple':
-        for x in range(rows):
-            if x%go_right==0:
-                factor+=1
-            if start_big_right_move>0 and start_big_right_move<30:
-                start_big_right_move+=1
-                if start_big_right_move%3==0:
-                    factor+=1
-            if start_big_left_move>0 and start_big_left_move<20:
-                start_big_left_move+=1
-                if start_big_left_move%2==0:
-                    ray_width_change+=1
-            elif start_big_left_move>=20 and start_big_left_move<30:
-                start_big_left_move+=1
-                if start_big_left_move<26:
-                    factor-=1
-                ray_width_change-=1
-            if x%ray_width_change_rate==0:
-                ray_width_change+=1
-            for y in range(cols):
-                i=random.randint(0,1)
-                if y in range((cols//2+20)-(ray_range//2)+factor-i,(cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-i):
-                    if x>((rows*end_of_face)+10) and make_ray_thinner<6:
-                        make_ray_thinner+=1
-                        if make_ray_thinner%3==0:
-                            ray_width_change+=1
-                    if thresh1[x,y]==[0] or thresh2[x,y]==0:
-                        if first_thrash==0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5,(cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
-                            start_big_right_move=1
-                            first_thrash=1
-                        if x>(rows*end_of_face) and last_thrash==0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5,(cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
-                            start_big_left_move=1
-                            last_thrash=1
-                        elif x-distance>10 and x>(rows*0.3) and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5,(cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
-                            factor-=1
-                            distance=x
-                    if thresh1[x,y]==[0] and (x>(rows*0.6) or x<(rows*0.2)):
-                        mask[x,y]=[25,25,25]
-                    elif thresh2[x,y]==[0] and (x>(rows*0.6) or x<(rows*0.2)):
-                        mask[x,y]=[100,100,100]
-                    elif thresh3[x,y]==[0]:
-                        mask[x,y]=[200,200,200]
+############CREATE SIMPLE MASK###############################################################
+        if mode != 'rainbow':
+            for x in range(rows):
+                if x % go_right == 0:
+                    factor += 1
+                if start_big_right_move > 0 and start_big_right_move < 30:
+                    start_big_right_move += 1
+                    if start_big_right_move % 3 == 0:
+                        factor += 1
+                if start_big_left_move > 0 and start_big_left_move < 20:
+                    start_big_left_move += 1
+                    if start_big_left_move % 2 == 0:
+                        ray_width_change += 1
+                elif start_big_left_move >= 20 and start_big_left_move < 30:
+                    start_big_left_move += 1
+                    if start_big_left_move < 26:
+                        factor -= 1
+                    ray_width_change -= 1
+                if x % ray_width_change_rate == 0:
+                    ray_width_change += 1
+                for y in range(cols):
+                    i = random.randint(0, 1)
+                    if y in range((cols//2+20)-(ray_range//2)+factor-i, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-i):
+                        if x > ((rows*end_of_face)+10) and make_ray_thinner < 6:
+                            make_ray_thinner += 1
+                            if make_ray_thinner % 3 == 0:
+                                ray_width_change += 1
+                        if thresh1[x, y] == [0] or thresh2[x, y] == 0:
+                            if first_thrash == 0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                                start_big_right_move = 1
+                                first_thrash = 1
+                            if x > (rows*end_of_face) and last_thrash == 0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                                start_big_left_move = 1
+                                last_thrash = 1
+                            elif x-distance > 10 and x > (rows*0.3) and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                                factor -= 1
+                                distance = x
+                        if thresh1[x, y] == [0] and (x > (rows*0.6) or x < (rows*0.2)):
+                            mask[x, y] = [25, 25, 25]
+                        elif thresh2[x, y] == [0] and (x > (rows*0.6) or x < (rows*0.2)):
+                            mask[x, y] = [100, 100, 100]
+                        elif thresh3[x, y] == [0]:
+                            mask[x, y] = [200, 200, 200]
+                        else:
+                            mask[x, y] = [250, 250, 250]
+                    elif i == 1 and (y == (cols//2+20)-(ray_range//2)+factor or y == (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                        mask[x, y] = [200, 200, 200]
                     else:
-                        mask[x,y]=[250,250,250]
-                elif i==1 and (y==(cols//2+20)-(ray_range//2)+factor or y==(cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
-                    mask[x,y]=[200,200,200]
+                        mask[x, y] = [0, 0, 0]
+################CREATE RAINBOW MASK###########################################################
+        mask2=np.zeros((rows, cols, channels))
+        ray_range=60#from which column to which column the ray spans
+        go_right=rows//ray_range
+        factor=0
+        ray_width=25
+        ray_width_change_rate=rows//(ray_width//2)
+        ray_width_change=0
+        first_thrash=0
+        last_thrash=0
+        start_big_right_move = 0
+        start_big_left_move = 0
+        distance = 0
+        make_ray_thinner = 0
+        color_change_rate = 3
+        colors = [[255, 127, 127], [255, 0, 127], [255, 0, 0], [0, 255, 0], [0, 255, 255], [0, 127, 127], [0, 0, 255]]
+        for x in range(rows):
+            if x % go_right == 0:
+                factor += 1
+            if start_big_right_move > 0 and start_big_right_move < 30:
+                start_big_right_move += 1
+                if start_big_right_move % 3 == 0:
+                    factor += 1
+            if start_big_left_move > 0 and start_big_left_move < 20:
+                start_big_left_move += 1
+                if start_big_left_move % 2 == 0:
+                    ray_width_change += 1
+            elif start_big_left_move >= 20 and start_big_left_move < 30:
+                start_big_left_move += 1
+                if start_big_left_move < 26:
+                    factor -= 1
+                ray_width_change -= 1
+            if x % ray_width_change_rate == 0:
+                ray_width_change += 1
+            col_change = 0
+            count = 0
+            for y in range(cols):
+                if(count % color_change_rate == 0) and col_change!=6:
+                    col_change += 1
+                i = random.randint(0, 1)
+                if y in range((cols//2+20)-(ray_range//2)+factor-i, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-i):
+                    if x > ((rows*end_of_face)+10) and make_ray_thinner < 6:
+                        make_ray_thinner += 1
+                        if make_ray_thinner % 3 == 0:
+                            ray_width_change += 1
+                    if thresh1[x, y] == [0] or thresh2[x, y] == 0:
+                        if first_thrash == 0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                            start_big_right_move = 1
+                            first_thrash = 1
+                        if x > (rows*end_of_face) and last_thrash == 0 and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                            start_big_left_move = 1
+                            last_thrash = 1
+                        elif x-distance > 10 and x > (rows*0.3) and y in range((cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor-5, (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                            factor -= 1
+                            distance = x
+                    if thresh1[x, y] == [0] and (x > (rows*0.6) or x < (rows*0.2)):
+                        mask2[x, y] = colors[col_change]
+                    elif thresh2[x, y] == [0] and (x > (rows*0.6) or x < (rows*0.2)):
+                        mask2[x, y] = colors[col_change]
+                    elif thresh3[x, y] == [0]:
+                        mask2[x, y] = colors[col_change]
+                    else:
+                        mask2[x, y] = colors[col_change]
+                elif i == 1 and (y == (cols//2+20)-(ray_range//2)+factor or y == (cols//2+20)-(ray_range//2)+(ray_width-ray_width_change)+factor):
+                    mask2[x, y] = colors[col_change]
                 else:
-                    mask[x,y] =[0,0,0]
-        mask=cv2.blur(mask,(7,7))
+                    mask2[x, y] = [0, 0, 0]
+                count += 1
+        mask2=cv2.blur(mask2,(7,7))
+        cv2.imshow('Mask 2',mask2)
+        mask = cv2.blur(mask, (7, 7))
         cv2.imshow('Mask',mask)
+        for x in range(rows):
+            for y in range(cols):
+                for c in range(channels):
+                    mask[x,y,c] = 0.7*mask[x,y,c]
+        for x in range(rows):
+            for y in range(cols):
+                for c in range(channels):
+                    mask2[x,y,c] = 0.3*mask2[x,y,c]
+        mask = mask+mask2
+        cv2.imshow('Mask sum',mask)
+        
         img = img.astype(np.uint8)
         mask = mask.astype(np.uint8)
         #img = cv2.addWeighted(img,0.7,mask,0.3,0)
         #Create a function that does the same without using addWeighted
-        blending_coef=0.5 #Change with user input later, between 0 and 1, 0 being only the mask and 1 being only the image
+        #blending coefficient between 0 and 1, 0 being only the mask and 1 being only the image
         for x in range(rows):
             for y in range(cols):
                 for c in range(channels):
